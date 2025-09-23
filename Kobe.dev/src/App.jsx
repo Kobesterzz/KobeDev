08347a18d39b2ff19201b97dbf279666c43e3fea
kobe shaw <kobeshaw330@gmail.com>
2025-09-23 11:40:35 -0500

    working on set up

---
+++ Kobe.dev/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import './css/App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+    </>
+  )
+}
+
+export default App
---
+++ Kobe.dev/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import '../css/index.css'
+
+function Header() {
+
+  return (
+    <>
+      <div className="header">
+        <div>
+            <div><h1>Kobe.dev</h1></div>
+        </div>
+        <div></div>
+      </div>
+    </>
+  )
+}
+
+export default Header
