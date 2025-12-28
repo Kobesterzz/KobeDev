0db3f57d1f9c770a762b661e54cd577da60d8f88
kobe shaw <kobeshaw330@gmail.com>
2025-12-27 22:32:53 -0600

    fixed display issues

--- Kobe.dev/src/App.jsx
+++ Kobe.dev/src/App.jsx
@@ -1,4 +1,3 @@
-import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Projects from './components/Projects'
 import MeExp from './components/MeExp'
@@ -8,17 +7,20 @@ import Top5 from "./components/top5"
 import ModelingPhoto from "./components/modelingAndPhoto"
 import './css/App.css'
 
-
 function App() {
+
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/projects" element={<h1>Projects</h1>} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/anime" element={<h1>Anime</h1>} />
-        <Route path="/modeling" element={<h1>Modeling</h1>} />
-      </Routes>
+      <Header/>
+      <Projects/>
+
+      <div className='contentPadding'>
+        <Experience/>
+        <RecentlyWatched/>
+        <Top5/>
+        <ModelingPhoto/>
+      </div>
+      
     </>
   )
 }
